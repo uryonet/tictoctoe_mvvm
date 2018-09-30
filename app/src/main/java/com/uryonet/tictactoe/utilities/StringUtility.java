3f3e1b63b577ad025da9be3969bb1d7e35d5a0d2
uryonet <uryonet@gmail.com>
2018-09-30 14:34:10 +0900

    StringUtilityクラスを追加

---
+++ app/src/main/java/com/uryonet/tictactoe/utilities/StringUtility.java
@@ -0,0 +1,7 @@
+package com.uryonet.tictactoe.utilities;
+
+public class StringUtility {
+    public static boolean isNullOrEmpty(String value) {
+        return value == null || value.length() == 0;
+    }
+}
