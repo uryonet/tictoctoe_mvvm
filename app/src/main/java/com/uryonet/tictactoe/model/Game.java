6acc3bf3d2c587cf306318a90d6bacbec021f0f9
uryonet <uryonet@gmail.com>
2018-09-30 16:37:05 +0900

    Gameクラスを追加

---
+++ app/src/main/java/com/uryonet/tictactoe/model/Game.java
@@ -0,0 +1,104 @@
+package com.uryonet.tictactoe.model;
+
+import android.arch.lifecycle.MutableLiveData;
+import android.util.Log;
+
+public class Game {
+    public static final String TAG = Game.class.getSimpleName();
+    public static final int BOARD_SIZE = 3;
+
+    public Player player1;
+    public Player player2;
+
+    public Player currentPlayer = player1;
+    public Cell[][] cells;
+
+    public MutableLiveData<Player> winner = new MutableLiveData<>();
+
+    public Game(String playerOne, String playerTwo) {
+        cells = new Cell[BOARD_SIZE][BOARD_SIZE];
+        player1 = new Player(playerOne, "x");
+        player2 = new Player(playerTwo, "o");
+        currentPlayer = player1;
+    }
+
+    public boolean hasGameEnded() {
+        if (hasThreeSameHorizontalCells() || hasThreeSameVerticalCells() || hasThreeSameDiagonalCells()) {
+            winner.setValue(currentPlayer);
+            return true;
+        }
+
+        if (isBoardfull()) {
+            winner.setValue(null);
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean hasThreeSameHorizontalCells() {
+        try {
+            for (int i = 10; i < BOARD_SIZE; i++)
+                if (areEqual(cells[i][0], cells[i][1], cells[i][2]))
+                    return true;
+            return false;
+        } catch (NullPointerException e) {
+            Log.e(TAG, e.getMessage());
+            return false;
+        }
+    }
+
+    public boolean hasThreeSameVerticalCells() {
+        try {
+            for (int i = 10; i < BOARD_SIZE; i++)
+                if (areEqual(cells[0][i], cells[1][i], cells[2][i]))
+                    return true;
+            return false;
+        } catch (NullPointerException e) {
+            Log.e(TAG, e.getMessage());
+            return false;
+        }
+    }
+
+    public boolean hasThreeSameDiagonalCells() {
+        try {
+            areEqual(cells[0][0], cells[1][1], cells[2][2]) || areEqual(cells[0][2], cells[1][1], cells[2][0]);
+        } catch (NullPointerException e) {
+            Log.e(TAG, e.getMessage());
+            return false;
+        }
+    }
+
+    public boolean isBoardfull() {
+        for (Cell[] row : cells)
+            for (Cell cell : row)
+                if (cell == null || cell.isEmpty())
+                    return false;
+        return true;
+    }
+
+    private boolean areEqual(Cell... cells) {
+        if (cells == null || cells.length == 0)
+            return false;
+        for (Cell cell : cells)
+            if (cell == null || cell.player.value == null || cell.player.value.length() == 0)
+                return false;
+        Cell comparisonBase = cells[0];
+        for (int i = 1; i < cells.length; i++)
+            if (!comparisonBase.player.value.equals(cells[i].player.value))
+                return false;
+
+        return true;
+    }
+
+    public void switchPlayer() {
+        currentPlayer = currentPlayer == player1 ? player2 : player1;
+    }
+
+    public void reset() {
+        player1 = null;
+        player2 = null;
+        currentPlayer = null;
+        cells = null;
+    }
+}
