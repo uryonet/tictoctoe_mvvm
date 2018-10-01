16c6e6224d1820aa11a821b27f80b5fd50bd0e4a
uryonet <uryonet@gmail.com>
2018-10-01 21:33:50 +0900

    メイン画面を追加

---
+++ app/src/main/java/com/uryonet/tictactoe/view/GameActivity.java
@@ -0,0 +1,51 @@
+package com.uryonet.tictactoe.view;
+
+import android.arch.lifecycle.ViewModelProvider;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.VisibleForTesting;
+import android.support.v7.app.AppCompatActivity;
+
+import com.uryonet.tictactoe.model.Player;
+import com.uryonet.tictactoe.viewmodel.GameViewModel;
+
+public class GameActivity extends AppCompatActivity {
+    private static final String GAME_BEGIN_DIALOG_TAG = "game_dialog_tag";
+    private static final String GAME_END_DIALOG_TAG = "game_end_dialog_tag";
+    private static final String NO_WINNER = "No one";
+    private GameViewModel gameViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        promptForPlayers();
+    }
+
+    public void promptForPlayers() {
+        GameBeginDialog dialog = GameBeginDialog.newInstance(this);
+        dialog.show(getSupportFragmentManager(), GAME_BEGIN_DIALOG_TAG);
+    }
+
+    public void onPlayersSet(String player1, String player2) {
+        initDataBinding(player1, player2);
+    }
+
+    private void initDataBinding(String player1, String player2) {
+        ActivityGameBinding activityGameBinding = DataBindingUtil.setContentView(this, R.layout.activity_game);
+        gameViewModel = ViewModelProvider.of(this).get(GameViewModel.class);
+        gameViewModel.init(player1, player2);
+        activityGameBinding.setGameViewModel(gameViewModel);
+        setUpOnGameEndListner();
+    }
+
+    private void setUpOnGameEndListner() {
+        gameViewModel.getWinner().observe(this, this::onGameWinnerChanged);
+    }
+
+    @VisibleForTesting
+    public void onGameWinnerChanged(Player winner) {
+        String winnerName = winner == null || isNullOrEmpty(winner.name) ? NO_WINNER : winner.name;
+        GameEndDialog dialog = GameEndDialog.newInstance(this, winnerName);
+        dialog.show(getSupportFragmentManager(), GAME_END_DIALOG_TAG);
+    }
+}
